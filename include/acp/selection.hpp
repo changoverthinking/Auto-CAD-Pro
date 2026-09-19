098651db33d5dc94798499a70934ea796a2543e5
duahauDevSolo <nductienth36@gmail.com>
2026-09-19 16:37:35 +0900

    feat(2d): add selection and hit testing

---
+++ include/acp/selection.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "acp/document.hpp"
+
+#include <optional>
+
+namespace acp::selection {
+
+struct Hit {
+    EntityId id{};
+    double distance{};
+    geo::Vec2 nearest{};
+};
+
+[[nodiscard]] double distance_to_entity(const Entity& entity, geo::Vec2 point) noexcept;
+[[nodiscard]] std::optional<Hit> hit_test(
+    const Document& document,
+    geo::Vec2 point,
+    double aperture) noexcept;
+
+} // namespace acp::selection
